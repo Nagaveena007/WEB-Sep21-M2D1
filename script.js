7d54cc2089aaf365deb959a01ccbe70605c38b88
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-10-05 15:51:29 +0200

    Java Script file

---
+++ script.js
@@ -0,0 +1,8 @@
+// MANDATORY JS EXERCISES (create a different file for this exercise)
+//     1) Save this array in a variable called genres: ["Comedy", "Drama", "Romance", "Horror", "Sci-Fi", "Documentary"]
+//     2) Create an unordered list using JavaScript and save it in a variable
+//     3) Cycle the array and create a list-item via JavaScript for every element
+//     4) Insert the genere as text in the list-item
+//     5) Append the list items in the unordered list
+//     6) Append the unordered list in the document. The list should appear in the page.
+
